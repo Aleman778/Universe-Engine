16b82ee95c1b767f28a62eadb056e4c9025b476c
Aleman778 <aleman778.maintheria@gmail.com>
2016-07-22 23:08:00 +0200

    Implemented OpenGL vertex buffer

---
+++ Universe Core/src/universe/opengl/GLVertexBuffer.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2016, Aleman778. All rights reserved.
+ * License terms are in the included LICENSE file.
+ */
+package universe.opengl;
+
+import static org.lwjgl.opengl.GL15.*;
+import universe.rendering.VertexBuffer;
+import universe.util.BufferUtils;
+
+
+/**
+ * OpenGL implementation for Vertex Buffers
+ * @author Aleman778
+ * @since Universe Core 1.0
+ */
+public final class GLVertexBuffer extends VertexBuffer {
+    /**
+     * The buffer object reference.
+     */
+    private final int buffer;
+    
+    /**
+     * Constructor.
+     */
+    public GLVertexBuffer() {
+        super();
+        buffer = glGenBuffers();
+    }
+
+    /**
+     * Set array data to this buffer.
+     * @param data the flaot array data to use
+     */
+    @Override
+    public void setArray(float[] data) {
+        bind();
+        glBufferData(GL_ARRAY_BUFFER, BufferUtils.createFloatBuffer(data), GL_STATIC_DRAW);
+        unbind();
+    }
+    
+    /**
+     * Bind the vertex buffer object.
+     */
+    @Override
+    public void bind() {
+        glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    }
+    
+    /**
+     * Unbind the vertex buffer object.
+     */
+    @Override
+    public void unbind() {
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    
+    /**
+     * Disposes the buffer.
+     * Note: buffer cannot be used after disposing.
+     */
+    @Override
+    public void dispose() {
+        glDeleteBuffers(buffer);
+    }
+}
