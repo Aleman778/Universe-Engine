cb2884b82ca89d26f428c41a6496b699b77a0327
Aleman778 <aleman778.maintheria@gmail.com>
2016-07-31 23:18:48 +0200

    Added Vertex Array and implemented Vertex Layout support

---
+++ Universe Core/src/universe/rendering/VertexLayout.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2016, Aleman778. All rights reserved.
+ * License terms are in the included LICENSE file.
+ */
+package universe.rendering;
+
+import java.util.ArrayList;
+
+/**
+ * Vertex layout class.
+ * @author Aleman778
+ * @since Universe Core 1.0
+ */
+public class VertexLayout {
+    
+    private int offset;
+    
+    private final int stride;
+    
+    private final ArrayList<VertexAttribute> attributes;
+
+    public VertexLayout(int count) {
+        attributes = new ArrayList<>();
+        stride = count * Float.BYTES;
+        offset = 0;
+    }
+    
+    public void push(String name, int count) {
+        VertexAttribute attri =
+                new VertexAttribute(name, count, offset * Float.BYTES, getSize());
+        attributes.add(attri);
+        offset += count;
+    }
+
+    public VertexLayout(int stride, ArrayList<VertexAttribute> attributes) {
+        this.stride = stride;
+        this.attributes = attributes;
+    }
+
+    public ArrayList<VertexAttribute> getAttributes() {
+        return attributes;
+    }
+
+    public int getStride() {
+        return stride;
+    }
+    
+    public int getSize() {
+        return attributes.size();
+    }
+}
