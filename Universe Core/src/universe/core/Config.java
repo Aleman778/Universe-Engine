b6906c5ad9eacf2d3bed67fffc89f164d4421526
Aleman778 <aleman778.maintheria@gmail.com>
2016-10-15 19:53:29 +0200

    Added configurations

---
+++ Universe Core/src/universe/core/Config.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016, Aleman778. All rights reserved.
+ * License terms are in the included LICENSE file.
+ */
+package universe.core;
+
+/**
+ * Universe application configurations.
+ * @author Aleman778
+ * @since Universe Core 1.0
+ */
+public class Config {
+    
+    /**
+     * Game title.
+     */
+    public String title = "Universe Engine";
+    
+    /**
+     * Display width.
+     */
+    public int width = 640;
+    
+    /**
+     * Display height.
+     */
+    public int height = 480;
+    
+    /**
+     * Fullscreen 
+     */
+    public boolean fullscreen = false;
+    
+    /**
+     * Rendering target reference.
+     */
+    public int renderType = Universe.NONE;
+}
